8804a8a9c83f218e8709c430f2b5352eeba9d1e9
U10416020 <wkps19961015@gmail.com>
2016-04-08 22:39:57 +0800

    Add annotations
    Add annotations

--- TestBubbleSort.java
+++ TestBubbleSort.java
@@ -3,17 +3,25 @@
 import java.util.*;
 
 public class TestBubbleSort{
+	//Main method
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter how many integers to sort: ");
-		int number = input.nextInt();
 		
+		//Ask user to input the numbers of integers
+		System.out.println("Enter how many integers to sort: ");
+		int number = input.nextInt();		
 		int[] array = new int[number];
+		
+		//Ask user to input the integers
 		System.out.println("Enter integers: ");
 		for(int i = 0; i < number; i++){
 			array[i] = input.nextInt();
 		}
+		
+		//Create a object of BubbleSort
 		BubbleSort bubble = new BubbleSort(array);
+		
+		//Diaplay the result
 		System.out.println("Bubble sort:");
 		for(int i = 0;i<number;i++){
 			System.out.print(bubble.getBubble(i) + " ");
